24c69486c6cfa3c415499dbe60b4d1f863a03c03
Cassio Izumi <izumicassio@gmail.com>
2023-09-20 12:10:58 -0300

    Added new functions to add border radius

---
+++ index.js
@@ -0,0 +1,15 @@
+function setUpperLeftBorder(value) {
+	document.querySelector("#Border-radius-Previewer").style.borderTopLeftRadius = `${value}px`;
+}
+
+function setUpperRightBorder(value) {
+	document.querySelector("#Border-radius-Previewer").style.borderTopRightRadius = `${value}px`;
+}
+
+function setBottomLeftBorder(value) {
+	document.querySelector("#Border-radius-Previewer").style.borderBottomLeftRadius = `${value}px`;
+}
+
+function setBottomRightBorder(value) {
+	document.querySelector("#Border-radius-Previewer").style.borderBottomRightRadius = `${value}px`;
+}
